8eb704bf171bd09696cebf91a9b5a2a72431b411
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-31 00:01:25 +0800

    指定ServiceProvider的路径

--- classes/ThemeServiceProvider.php
+++ classes/ThemeServiceProvider.php
@@ -9,7 +9,7 @@ class ThemeServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-theme', null, dirname(__DIR__));
+        $this->package('ecjia/app-theme');
     }
     
     public function register()
