4b42cc634c9d82728190780bdb5a751af2c3d7de
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-22 15:01:00 +0800

    更新主题框架配置格式

--- configs/taxonomy.php
+++ configs/taxonomy.php
@@ -3,45 +3,47 @@
 // ===============================================================================================
 // -----------------------------------------------------------------------------------------------
 // TAXONOMY OPTIONS
+// $options
 // -----------------------------------------------------------------------------------------------
 // ===============================================================================================
-$options     = array();
+return array(
 
-// -----------------------------------------
-// Taxonomy Options                        -
-// -----------------------------------------
-$options[]   = array(
-  'id'       => '_custom_taxonomy_options',
-  'taxonomy' => 'taxonomy_name', // category, post_tag or your custom taxonomy name
-  'fields'   => array(
-
+    // -----------------------------------------
+    // Taxonomy Options                        -
+    // -----------------------------------------
     array(
-      'id'    => 'section_1_text',
-      'type'  => 'text',
-      'title' => 'Text Field',
+        'id'       => '_custom_taxonomy_options',
+        'taxonomy' => 'taxonomy_name', // category, post_tag or your custom taxonomy name
+        'fields'   => array(
+
+            array(
+                'id'    => 'section_1_text',
+                'type'  => 'text',
+                'title' => 'Text Field',
+            ),
+
+            array(
+                'id'    => 'section_1_textarea',
+                'type'  => 'textarea',
+                'title' => 'Textarea Field',
+            ),
+
+        ),
     ),
 
-    array(
-      'id'    => 'section_1_textarea',
-      'type'  => 'textarea',
-      'title' => 'Textarea Field',
-    ),
 
-  ),
+    array(
+        'id'       => '_custom_taxonomy_options',
+        'taxonomy' => 'another_taxonomy_name', // category, post_tag or your custom taxonomy name
+        'fields'   => array(
+
+            array(
+                'id'    => 'section_1_text',
+                'type'  => 'text',
+                'title' => 'Text Field',
+            ),
+
+        ),
+    ),
 );
 
-$options[]   = array(
-  'id'       => '_custom_taxonomy_options',
-  'taxonomy' => 'another_taxonomy_name', // category, post_tag or your custom taxonomy name
-  'fields'   => array(
-
-    array(
-      'id'    => 'section_1_text',
-      'type'  => 'text',
-      'title' => 'Text Field',
-    ),
-
-  ),
-);
-
-CSFramework_Taxonomy::instance( $options );
