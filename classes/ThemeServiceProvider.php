d26c067506392cc90f464a53acc8d215f71fe611
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-22 14:36:42 +0800

    封装ecjia主题选项表存取方法

--- classes/ThemeServiceProvider.php
+++ classes/ThemeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Ecjia\App\Theme;
 
+use Ecjia\App\Theme\ThemeOption\ThemeOption;
 use Royalcms\Component\App\AppParentServiceProvider;
 
 class ThemeServiceProvider extends  AppParentServiceProvider
@@ -14,9 +15,32 @@ class ThemeServiceProvider extends  AppParentServiceProvider
     
     public function register()
     {
-        
+        $this->registerThemeOption();
+
+        $this->loadAlias();
     }
-    
-    
+
+    /**
+     * Register the region
+     * @return \Ecjia\App\Theme\ThemeOption\ThemeOption
+     */
+    public function registerThemeOption()
+    {
+        $this->royalcms->bindShared('ecjia.theme.option', function($royalcms){
+            return new ThemeOption();
+        });
+    }
+
+    /**
+     * Load the alias = One less install step for the user
+     */
+    protected function loadAlias()
+    {
+        $this->royalcms->booting(function()
+        {
+            $loader = \Royalcms\Component\Foundation\AliasLoader::getInstance();
+            $loader->alias('ecjia_theme_option', 'Ecjia\App\Theme\Facades\EcjiaThemeOption');
+        });
+    }
     
 }
