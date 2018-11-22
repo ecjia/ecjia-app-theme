dde75f114a475236174c102a3f9081765ccc6fa4
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-22 15:05:25 +0800

    添加主题选项菜单

---
+++ admin_option.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/11/22
+ * Time: 15:03
+ */
+
+defined('IN_ECJIA') or exit('No permission resources.');
+
+
+class admin_option extends ecjia_admin
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+
+    public function init()
+    {
+
+    }
+
+
+}
